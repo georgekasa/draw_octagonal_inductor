9b721e75a192fd8d1978a5329dd7dfb72231ae82
george <79354220+georgekasa@users.noreply.github.com>
2023-12-02 16:22:29 +0200

    Update spiral_Ind_script.py

--- spiral_Ind_script.py
+++ spiral_Ind_script.py
@@ -17,7 +17,8 @@ def draw_octagonal_inductor2(turns, spacing, width, height):
     original_x = np.zeros(9)
     original_y = np.zeros(9)
     for turn in range(turns):
-        radius = (turns - turn) * (spacing) * min(width, height) / (2 * turns)
+        radius = (turns - turn) * (spacing) * width/ (2 * turns)
+	radiusY = (turns - turn) * (spacing) * height / (2 * turns)
         rotation = np.pi / 8
        # if turn % 2 != 0:
             #rotation = -rotation
@@ -30,7 +31,7 @@ def draw_octagonal_inductor2(turns, spacing, width, height):
            
                 xPoints[i] = centerX + radius * np.cos(angle)
                 
-                yPoints[i] = centerY + radius * np.sin(angle)
+                yPoints[i] = centerY + radiusY * np.sin(angle)
             print(xPoints)
             print(yPoints)
             #print("##################")
